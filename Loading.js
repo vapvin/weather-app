fdda3a6f8a8884b8a74bd783c615d586306e8f84
vapvin <tpehdj12@daum.net>
2019-10-05 01:48:53 +0900

    Loading Screen

---
+++ Loading.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Getting the fucking weather</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#fceabb"
+  },
+  text: {
+    color: "#4c4c4c",
+    fontSize: 30
+  }
+});
